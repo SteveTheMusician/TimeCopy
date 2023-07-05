d482526637e9f09d93ef768fc644d2dddf8ad776
Steve <admin@mbp-von-conrad.prime-force-frankfurt.de>
2023-07-05 15:39:30 +0200

    add style, logo, input feelds, functions

--- extension/libraries/amag-protime/amag-protime.js
+++ extension/libraries/amag-protime/amag-protime.js
@@ -1,3 +1,6 @@
 document.getElementById('WD0172').value = "21053";
-
+document.getElementById("WD0172").dispatchEvent(new KeyboardEvent('keydown', {'key': 'Enter'}));
+document.getElementById("WD0172").dispatchEvent(new KeyboardEvent('keyup', {'key': 'Enter'}));
 var test = document.getElementById('WD0172').value;
+document.dispatchEvent(new KeyboardEvent('keydown', {'key': 'Enter'}));
+document.dispatchEvent(new KeyboardEvent('keyup', {'key': 'Enter'}));
