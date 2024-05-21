f087f24a8e5619c10cd2bedfab96917bc2eff370
Steve <stefan.pietrowski@prime-force.com>
2024-05-21 15:59:13 +0200

    timesheet icons,styles

---
+++ extension/components/timesheetItems/timesheetItems.js
@@ -0,0 +1,24 @@
+export function timesheetItem (){
+    
+    let timesheetItem = `<label class="config-item dFlex">
+    <div class="config-item-main-container dFlex">
+      <div class="config-item-radio-container dFlex">
+        <label class="radio-custom-container dFlex">
+          <input type="radio" class="radio-default" value="timesheetfilter-tobiasexcel" name="timesheet-filter"/>
+          <span class="checkmark"></span>
+        </label>
+      </div>
+      <div class="config-item-main flex">
+        <p class="text-label">Tobias Excel</p>
+      </div>  
+    </div>
+    <div class="config-item-action-container flex">
+      <button class="button-primary" id="tobiasFilterInfo">
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+        <svg width="100%" height="100%" viewBox="0 0 1000 1000" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+            <path id="information_2" d="M499.925,1000C224.002,999.959 0,775.933 0,500C0,224.042 224.042,0 500,0C775.958,0 1000,224.042 1000,500C1000,775.958 775.958,1000 500,1000L499.925,1000ZM589.925,900.09C773.016,859.053 910,695.397 910,500C910,273.715 726.285,90 500,90C273.715,90 90,273.715 90,500C90,695.344 226.909,858.963 409.925,900.056L409.925,540C409.925,490.328 450.253,450 499.925,450C549.597,450 589.925,490.328 589.925,540L589.925,900.09ZM590.075,270C590.075,319.672 549.747,360 500.075,360C450.403,360 410.075,319.672 410.075,270C410.075,220.328 450.403,180 500.075,180C549.747,180 590.075,220.328 590.075,270Z"/>
+        </svg>
+      </button>
+    </div>
+  </label>`
+}
