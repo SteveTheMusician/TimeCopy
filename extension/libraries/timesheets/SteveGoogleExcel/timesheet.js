8ff9d14b2fa02324db2eb1ad91e03de669a572a8
Steve <stefan.pietrowski@prime-force.com>
2024-06-18 13:56:26 +0200

    add disabled class, renaming, cleanup

--- extension/libraries/timesheets/SteveGoogleExcel/timesheet.js
+++ extension/libraries/timesheets/SteveGoogleExcel/timesheet.js
@@ -1,6 +1,6 @@
-import { bookingsheets } from "../../bookingplattforms/bookingplattforms.js"
+import { bookingplattforms } from "../../bookingplattforms/bookingplattforms.js"
 
 export function timesheet_SteveGoogleExcel(bookingPlattform,clipboarsString,dev_pttest) {
     alert('Steve Google Excel'+bookingPlattform)
-    bookingsheets(bookingPlattform,"some data")
+    bookingplattforms(bookingPlattform,"some data")
 }
