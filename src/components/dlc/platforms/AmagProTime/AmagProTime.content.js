aee839de379608a59b10cf5dfe79ce41fb2bee8e
Steve <stefan.pietrowski@prime-force.com>
2024-09-15 23:40:36 +0200

    protime dlc wording

--- src/components/dlc/platforms/AmagProTime/AmagProTime.content.js
+++ src/components/dlc/platforms/AmagProTime/AmagProTime.content.js
@@ -2,7 +2,7 @@ export const customContent = `<div class="dlc-platform_custom-content">
 <p class="text-label">Funktionen</p>
 <div class="dlc-platform_custom-content-row dFlex">
     <div class="row-cell-left">
-        <p class="subtext">ProTime Test-Funktion </p>
+        <p class="subtext">ProTime Test </p>
     </div>
     <div class="row-cell-right">
         <label class="switch">
