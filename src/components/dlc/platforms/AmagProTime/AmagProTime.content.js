68ed6c85f8141f42cbc8e0a8faf3611454a4fb7a
Steve <stefan.pietrowski@prime-force.com>
2024-09-16 00:02:33 +0200

    fix custom content functions row

--- src/components/dlc/platforms/AmagProTime/AmagProTime.content.js
+++ src/components/dlc/platforms/AmagProTime/AmagProTime.content.js
@@ -1,19 +1,33 @@
 export const customContent = `<div class="dlc-platform_custom-content">
-<p class="text-label">Funktionen</p>
-<div class="dlc-platform_custom-content-row dFlex">
-    <div class="row-cell-left">
-        <p class="subtext">ProTime Test </p>
+    <p class="text-label">Funktionen</p>
+    <div class="dlc-platform_custom-content-row dFlex">
+        <div class="row-cell-left">
+            <p class="subtext">ProTime Test </p>
+        </div>
+        <div class="row-cell-right">
+            <label class="switch">
+                <input type="checkbox" class="" id="check_showProTimetestButton" />
+                <span class="switch_slider"></span>
+            </label>
+        </div>
     </div>
-    <div class="row-cell-right">
-        <label class="switch">
-            <input type="checkbox" class="" id="check_showProTimetestButton" />
-            <span class="switch_slider"></span>
-        </label>
+    <div class="dlc-platform_custom-content-row dFlex">
+        <div class="row-cell-left">
+            <p class="subtext">Verzugs-Timer </p>
+        </div>
+        <div class="row-cell-right">
+            <select name="dlc_select_proTimeDelayTimer" id="dlc-select-protimedelaytimer">
+                <option value="150" >150 ms</option> 
+                <option value="250" selected>250 ms</option>
+                <option value="350" >350 ms</option>
+                <option value="500" >500 ms</option>
+                <option value="500" >750 ms</option>
+                <option value="500" >1000 ms</option>     
+            </select>
+        </div>
     </div>
-<div>
-
-
 </div>`
+
 export function CustomFunction(){
     console.log('protime custom function')
 
