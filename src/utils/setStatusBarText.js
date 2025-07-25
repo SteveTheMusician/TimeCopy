7eb527c9cd21c478edf388bb5a3335127a6d7603
SteveTheMusician <stevearizona98@gmail.com>
2025-07-25 15:50:46 +0200

    Statusbar feedback fix on app error, statustext to language, filter dlc text

--- src/utils/setStatusBarText.js
+++ src/utils/setStatusBarText.js
@@ -1,22 +1,20 @@
 export function setStatusBarText(statusText,state) {
 
-        let defaultText = "Bereit"
-        let statusTextElem = document.getElementById('statusBarStatusText')
-
-        if(statusTextElem.innerHTML !== defaultText && state !== 'reset') {
-            setTimeout(function(){
-                statusTextElem.innerHTML = statusText
-            },300)
-        } else {
+    let defaultText = window.language.statusbartext_stby
+    let statusTextElem = document.getElementById('statusBarStatusText')
+    if(statusTextElem.innerHTML !== defaultText && state !== 'reset') {
+        setTimeout(function(){
             statusTextElem.innerHTML = statusText
-        }
-
-        if(state === 'timeout') {
-            setTimeout(function(){
-                statusTextElem.innerHTML = defaultText
-            },900)
-        }
-        if(state === 'reset') {
+        },300)
+    } else {
+        statusTextElem.innerHTML = statusText
+    }
+    if(state === 'timeout') {
+        setTimeout(function(){
             statusTextElem.innerHTML = defaultText
-        }
+        },900)
+    }
+    if(state === 'reset') {
+        statusTextElem.innerHTML = defaultText
+    }
 }
